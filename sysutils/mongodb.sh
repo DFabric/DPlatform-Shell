4504be8b9eb6b3a8aad86a8b6aa4b6b386294ca9
JulR <jul.reich43@opmbx.org>
2015-11-12 21:04:45 +0100

    Mongo 3.0 in Debian/Red Hat

--- sysutils/mongodb.sh
+++ sysutils/mongodb.sh
@@ -4,7 +4,7 @@
 if [ $ARCH = arm ]
   then $install mongodb
 
-# Debian (apt) based OS
+# Debian (deb) based OS
 elif [ $PKG = deb ]
   then
   # Ubuntu repository
@@ -13,22 +13,22 @@ elif [ $PKG = deb ]
     then echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
   # All other Debian based distributions
   else
-    echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+    echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
   fi
   apt-get update
   apt-get install -y mongodb-org
 
-# Red Hat (RPM) based OS
+# Red Hat (rpm) based OS
 elif [ $PKG = rpm ]
-  then echo '[mongodb-org-3.0]' > /etc/yum.repos.d/mongodb-org-3.2.repo
-  echo 'name=MongoDB Repository' >> /etc/yum.repos.d/mongodb-org-3.2.repo
+  then echo '[mongodb-org-3.0]' > /etc/yum.repos.d/mongodb-org-3.0.repo
+  echo 'name=MongoDB Repository' >> /etc/yum.repos.d/mongodb-org-3.0.repo
   if grep 'Amazon' /etc/issue 2>/dev/null
-    echo 'baseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/3.0/x86_64/' >> /etc/yum.repos.d/mongodb-org-3.2.repo
+    then echo 'baseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/3.0/x86_64/' >> /etc/yum.repos.d/mongodb-org-3.0.repo
   else
-    echo 'baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.0/x86_64/' >> /etc/yum.repos.d/mongodb-org-3.2.repo
+    echo 'baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.0/x86_64/' >> /etc/yum.repos.d/mongodb-org-3.0.repo
   fi
-  echo 'gpgcheck=0' >> /etc/yum.repos.d/mongodb-org-3.2.repo
-  echo 'enabled=1' >> /etc/yum.repos.d/mongodb-org-3.2.repo
+  echo 'gpgcheck=0' >> /etc/yum.repos.d/mongodb-org-3.0.repo
+  echo 'enabled=1' >> /etc/yum.repos.d/mongodb-org-3.0.repo
 
   yum install -y mongodb-org --enablerepo=epel
 
