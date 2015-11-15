47ac1aaa55e577fbfa211b2818fd11b09fd3182f
JulR <jul.reich43@opmbx.org>
2015-11-15 17:37:27 +0100

    Add NodeBB and Stringer

---
+++ apps/nodebb.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+MONGODB_VERSION=26
+. sysutils/nodejs.sh
+. sysutils/mongodb.sh
+
+# https://docs.nodebb.org/en/latest/installing/os.html
+
+## Installing NodeBB
+# Install the base software stack
+$install mongodb imagemagick git build-essential
+
+# Clone the repository
+git clone -b v0.9.x https://github.com/NodeBB/NodeBB.git nodebb
+
+# Obtain all dependencies required by NodeBB via NPM
+cd nodebb
+npm install --production
+
+# Install NodeBB by running the app with –setup flag
+./nodebb setup
+
+# Run the NodeBB forum
+./nodebb start
+
+whiptail --msgbox "NodeBB successfully installed!
+
+Open http://your_ip:4567 in your browser" 12 60
+# https://www.npmjs.com/package/nodebb-plugin-blog-comments
