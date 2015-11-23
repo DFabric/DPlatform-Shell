47299f548f0fcd62560798c02f7c0877e12e9d0b
Julien Reichardt <jul.reich43@opmbx.org>
2015-11-23 19:32:30 +0100

    Access to web UI through NGINX

--- apps/mailpile.sh
+++ apps/mailpile.sh
@@ -30,6 +30,44 @@ git pull
 # update any submodules (documentation, plug-ins)
 git submodule update
 
+# https://github.com/mailpile/Mailpile/wiki/Accessing-The-GUI-Over-Internet
+$install nginx
+
+cat > /etc/nginx/sites-enabled/mailpile <<EOF
+server {
+  listen 80;
+  server_name server.com;
+  return 301 https://$server_name$request_uri;
+}
+
+server {
+  listen 443 ssl;
+  server_name server.com;
+
+  # see https://raymii.org/s/tutorials/Strong_SSL_Security_On_nginx.html
+  # for notes on the good SSL on nginx
+  ssl_certificate /etc/nginx/ssl/server.com.crt;
+  ssl_certificate_key /etc/nginx/ssl/server.com.key;
+  ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+  ssl_ciphers 'EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH';
+  ssl_prefer_server_ciphers   on;
+  ssl_session_cache shared:SSL:10m;
+  add_header Strict-Transport-Security "max-age=31536000; includeSubdomains";
+  ssl_dhparam /etc/nginx/ssl/dhparam.pem;
+
+  location / {
+    access_log /var/log/nginx/mailpile_access.log;
+    error_log /var/log/nginx/mailpile_error.log info;
+
+    proxy_pass http://127.0.0.1:33411;
+    proxy_set_header X-Forwarded-Host $host;
+    proxy_set_header X-Forwarded-Server $host;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+  }
+}
+EOF
+
+
 whiptail --msgbox "Mailpile successfully installed!
 You should need to open port 33411 and 993
 Open http://$DOMAIN:33411 in your browser
