bf307e2446356eb73677a353ac1eb74b35c40bd3
Julien Reichardt <jul.reich43@opmbx.org>
2015-12-28 21:21:29 +0100

    Update reaction.sh

--- apps/reaction.sh
+++ apps/reaction.sh
@@ -4,18 +4,24 @@
 
 # Install Meteor
 # https://github.com/4commerce-technologies-AG/meteor
-if [ $ARCH = arm* ]
-  then cd $HOME
+case $ARCH in
+  arm*) cd $HOME
   git clone --depth 1 https://github.com/4commerce-technologies-AG/meteor.git
   # Check installed version, try to download a compatible pre-built dev_bundle and finish the installation
   meteor/meteor --version
-  cd $DIR
-else
-  curl https://install.meteor.com/ | sh
-fi
+  cd $DIR;;
+  *) curl https://install.meteor.com/ | sh;;
+esac
 # Install Reaction
 git clone https://github.com/reactioncommerce/reaction.git
 
 # Start the latest release
 cd reaction && git checkout master # default branch is development
-reaction
+./reaction
+
+whiptail --msgbox "Reaction Commerce successfully installed!
+To run Reaction:
+cd reaction
+./reaction
+
+Open http://$IP:3000 in your browser" 12 48
