d0cf05e998e7dc9da81b0ddc02e2b29ce3a076b0
Julien Reichardt <jul.reich43@opmbx.org>
2015-12-28 22:45:46 +0100

    "meteor" instead "reaction" for arm compatibility

--- apps/reaction.sh
+++ apps/reaction.sh
@@ -19,11 +19,11 @@ git clone https://github.com/reactioncommerce/reaction.git
 
 # Start the latest release
 cd reaction && git checkout master # default branch is development
-./reaction
+meteor #./reaction
 
 whiptail --msgbox "Reaction Commerce successfully installed!
 To run Reaction:
 cd reaction
-./reaction
+meteor
 
 Open http://$IP:3000 in your browser" 12 48
