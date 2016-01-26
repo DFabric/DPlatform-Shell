b4c255f106eb1c1226e20dbe71042e1106e11a17
Julien Reichardt <jul.reich43@opmbx.org>
2016-01-26 23:53:36 +0100

    Don't use nvm because shell restarting needed

--- sysutils/nodejs.sh
+++ sysutils/nodejs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
-nvm install 5
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+#nvm install 5
+$install nodejs
 
 echo "Node.js installed"
