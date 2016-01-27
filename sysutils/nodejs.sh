9fd3a09ef0fcc1bf3f32b9a43d55cd9c12d52a91
Julien Reichardt <jul.reich43@opmbx.org>
2016-01-27 09:28:44 +0100

    Update node.js to 4.x for arm compatibility

--- sysutils/nodejs.sh
+++ sysutils/nodejs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # https://github.com/nodesource/distributions/
-curl -sL https://$PKG.nodesource.com/setup_0.10 | bash -
+curl -sL https://$PKG.nodesource.com/setup_4.x | bash -
 $install nodejs
 
 echo "Node.js installed"
