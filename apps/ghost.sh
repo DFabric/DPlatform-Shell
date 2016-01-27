2b4d2a6fe5359fa02aee74763774c0583f7b84df
Julien Reichardt <jul.reich43@opmbx.org>
2016-01-27 01:01:41 +0100

    Update instructions

--- apps/ghost.sh
+++ apps/ghost.sh
@@ -2,30 +2,24 @@
 
 . sysutils/nodejs.sh
 
-npm install nave -g
-nave usemain 0.12.7
-
 $install unzip
 
 # http://support.ghost.org/installing-ghost-linux/
 # https://www.howtoinstallghost.com/vps-manual/
 
 ## Download and Install Ghost
-# Grab the latest version of Ghost from Ghost.org
+# Get the latest version of Ghost from Ghost.org
 curl -L https://ghost.org/zip/ghost-latest.zip -o ghost.zip
 
 # Unzip Ghost into the recommended install folder location /var/www/ghost
-
-mkdir /var/www/
+mkdir -p /var/www/
 unzip -uo ghost.zip -d /var/www/ghost
-
 rm ghost.zip
-# Move to the new ghost directory, and install Ghost production dependencies
 
+# Move to the new ghost directory, and install Ghost production dependencies
 cd /var/www/ghost && npm install --production
 
 ## Configure Ghost
-
 cp config.example.js config.js
 sed -i "s/host: '127.0.0.1',/host: '0.0.0.0',/g" config.js
 
