9d0bbdb8041fbbb9fed91c3495784655c58547ae
Julien Reichardt <jul.reich43@opmbx.org>
2016-02-23 21:30:35 +0100

    Add supervisor process

--- apps/JS_Bin.sh
+++ apps/JS_Bin.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+
 . sysutils/nodejs.sh
+
+[ $1 = update ] && npm udpate jsbin
+[ $1 = remove ] && "npm uninstall jsbin" && "sh $DIR/sysutils/supervisor remove JS_Bin" && whiptail --msgbox "JS_Bin removed!" 8 32 && break
+
+# Installation
 npm install -g jsbin
-jsbin
+
+# Add supervisor process and run the server
+sh $DIR/sysutils/supervisor.sh JS_Bin jsbin /
 
 whiptail --msgbox "JSBin successfully installed!
 
