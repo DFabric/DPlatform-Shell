6144516e6057112be540eeeb03e5812bde6d9ed5
Julien Reichardt <jul.reich43@opmbx.org>
2016-02-24 11:24:25 +0100

    Capital letters

--- apps/EtherCalc.sh
+++ apps/EtherCalc.sh
@@ -2,8 +2,8 @@
 
 . sysutils/nodejs.sh
 
-[ $1 = update ] && npm udpate ethercalc
-[ $1 = remove ] && "npm uninstall ethercalc" && "sh $DIR/sysutils/supervisor remove EtherCalc" && whiptail --msgbox "EtherCalc removed!" 8 32 && break
+[ $1 = update ] && npm udpate EtherCalc
+[ $1 = remove ] && "npm uninstall EtherCalc" && "sh $DIR/sysutils/supervisor remove EtherCalc" && whiptail --msgbox "EtherCalc removed!" 8 32 && break
 
 # Globa installation
 npm i -g ethercalc
