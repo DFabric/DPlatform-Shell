2422a681b68a43479ca2a78f9ed1604756dce523
j8r <jul.reich43@protonmail.ch>
2016-02-24 22:35:29 +0100

    Add supervisor process

--- apps/Gogs.sh
+++ apps/Gogs.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+cd
+[ $1 = update ] && whiptail --msgbox "Not availabe yet!" 8 32 && break
+[ $1 = remove ] && "sh $DIR/sysutils/supervisor remove Gogs" && rm -rf gogs && whiptail --msgbox "Gogs removed!" 8 32 && break
+
 # Prerequisites
-$install sqlite3 git unzip
+$install sqlite3 git
 
 # Get the latest Gogs release
 ver=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/gogits/gogs/releases/latest)
@@ -9,19 +13,21 @@ ver=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/gogits/gogs/r
 # Only keep the version number in the url
 ver=$(echo $ver | awk '{ver=substr($0, 46); print ver;}')
 
-[ $ARCH = 86 ] && ARCH=386
-cd
-if [ $ARCH = arm ]
-  then wget https://github.com/gogits/gogs/releases/download/v$ver/raspi2.zip
-  unzip raspi2.zip
-  rm raspi2.zip
-else
-  wget https://github.com/gogits/gogs/releases/download/v$ver/linux_$ARCH.zip
-  unzip linux_$ARCH.zip
-  rm linux_$ARCH.zip
+if [ $ARCH = amd64 ] || [ $ARCH = 86 ]
+then
+  [ $ARCH = 86 ] && ARCH=386
+  wget https://cdn.gogs.io/gogs_v0.8.43_linux_$ARCH.tar.gz
+  tar -zxvf gogs_v0.8.43_linux_$ARCH.tar.gz
+  rm gogs_v0.8.43_linux_$ARCH.tar.gz
+elif [ $ARCH = arm ]
+then
+  wget https://cdn.gogs.io/gogs_v${ver}_raspi2.zip
+  unzip gogs_v${ver}_raspi2.zip
+  rm gogs_v${ver}_raspi2.zip
 fi
 
-cd gogs && ./gogs web
+# Add supervisor process, configure and start GitLab
+sh $DIR/sysutils/supervisor.sh Gogs "./gogs web" $HOME/gogs
 
 whiptail --msgbox "Gogs successfully installed!
 
