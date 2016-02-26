2ed76a70fdac0345608d1fc8fd2ee0b5f72e00d3
j8r <jul.reich43@protonmail.ch>
2016-02-26 14:15:44 +0100

    Add supervisor process

--- apps/EtherDraw.sh
+++ apps/EtherDraw.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
+if [ $1 = update ]
+then
+  cd ~/draw
+  git pull
+  whiptail --msgbox "EtherDraw updated!" 8 32
+  break
+fi
+[ $1 = remove ] && sh sysutils/supervisor.sh remove EtherDraw && rm -rf draw && whiptail --msgbox "EtherDraw removed!" 8 32 && break
+
+. sysutils/NodeJS.sh
+
+cd
 # Install Requirements
 $install libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++
 
-cd
 # Install EtherDraw
-git clone git://github.com/JohnMcLear/draw.git
+git clone https://github.com/JohnMcLear/draw
 
+#prepare the enviroment
 cd draw
-bin/run.sh
+sh bin/installDeps.sh
+
+# Add supervisor process and run the server
+sh $DIR/sysutils/supervisor.sh EtherDraw "node server.js" $HOME/draw
 
 whiptail --msgbox "EtherDraw successfully installed!
 
--- apps/Etherpad.sh
+++ apps/Etherpad.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
+if [ $1 = update ]
+then
+  cd ~/etherpad-lite
+  git pull
+  whiptail --msgbox "Etherpad updated!" 8 32
+  break
+fi
+[ $1 = remove ] && sh sysutils/supervisor.sh remove Etherpad && rm -rf etherpad-lite && whiptail --msgbox "Etherpad removed!" 8 32 && break
+
 . sysutils/NodeJS.sh
 
+cd
+# gzip, git, curl, libssl develop libraries, python and gcc needed
 if [ $PKG = apt ]
   then apt-get install gzip git curl python libssl-dev pkg-config build-essential
 elif [ $PKG = rpm ]
   then yum install gzip git curl python openssl-devel && yum groupinstall "Development Tools"
 fi
-cd
-git clone git://github.com/ether/etherpad-lite.git
-cd etherpad-lite
-bin/run.sh
+git clone https://github.com/ether/etherpad-lite
+
+# Add supervisor process and run the server
+sh $DIR/sysutils/supervisor.sh Etherpad 'sh -c "bin/run.sh --root"' $HOME/etherpad-lite
 
 whiptail --msgbox "Etherpad successfully installed!
 
-To start Etherpad, run:
-cd etherpad-lite && bin/run.sh
-
-Open http://$IP:9001 in your browser." 12 64
+Open http://$IP:9001 in your browser." 10 64
