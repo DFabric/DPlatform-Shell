437804bebb170c21f1f3b565d13e4b250ba1a1b3
Julien Reichardt <jul.reich43@opmbx.org>
2016-03-09 23:52:35 +0100

    IPv4 detection URL change

--- dplatform.sh
+++ dplatform.sh
@@ -15,7 +15,7 @@ cd $DIR
 git pull
 
 # Detect IP
-IPv4=$(wget -qO- https://www.cuby-hebergs.com/ip) || IPv4=$(wget -qO- ipv4.icanhazip.com)
+IPv4=$(wget -qO- http://ip4.cuby-hebergs.com/) || IPv4=$(wget -qO- ipv4.icanhazip.com)
 IPv6=$(ip addr | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d' | tail -n 2 | head -n 1)
 LOCALIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 # Set default IP to IPv4 unless IPv6 is available
