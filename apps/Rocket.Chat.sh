6f7e5853581d73a38b01c20ca542ca96db6869dd
j8r <jul.reich43@protonmail.ch>
2016-03-19 20:23:08 +0100

    SystemD final fix

--- apps/Rocket.Chat.sh
+++ apps/Rocket.Chat.sh
@@ -2,7 +2,6 @@
 
 # Remove the old server executables
 [ $1 = update ] || [ $1 = remove ] && rm -rf ~/Rocket.Chat
-[ $1 = remove ] && [ $ARCH = arm ] && "supervisorctl remove Rocket.chat; rm -f /etc/supervisor/conf.d/Rocket.Chat.conf; supervisorctl reread; supervisorctl update"  && whiptail --msgbox "Rocket.Chat removed!" 8 32 && break
 [ $1 = remove ] && sh sysutils/services.sh remove Rocket.Chat && whiptail --msgbox "Rocket.Chat removed!" 8 32 && break
 
 . sysutils/MongoDB.sh
@@ -67,7 +66,6 @@ then
         replSetName:  "001-rs"' >> /etc/mongod.conf
   fi
   service mongod restart
-  mongo
 
   # Start the MongoDB shell and initiate the replica set
   mongo rs.initiate
@@ -79,6 +77,8 @@ then
   #  "info" : "Config now saved locally.  Should come online in about a minute.",
   #  "ok" : 1
   # }
+  $ReplicaSet="
+Environment=MONGO_OPLOG_URL=mongodb://localhost:27017/local"
 fi
 
 # Set environment variables
@@ -92,56 +92,18 @@ then
   sh $DIR/sysutils/services.sh Rocket.Chat "/usr/bin/node main.js" "$HOME/Rocket.Chat
 Environment=ROOT_URL=http://$IP:$port/
 Environment=PORT=$port
-Environment=MONGO_URL=mongodb://localhost:27017/rocketchat
-Environment=MONGO_OPLOG_URL=mongodb://localhost:27017/local"
+Environment=MONGO_URL=mongodb://localhost:27017/rocketchat$ReplicSet"
+
+elif [ $ARCH = arm ]
+then
+  sh $DIR/sysutils/services.sh Rocket.Chat "$HOME/meteor/dev_bundle/bin/node main.js" "$HOME/Rocket.Chat
+Environment=ROOT_URL=http://$IP:$port/
+Environment=PORT=$port
+Environment=MONGO_URL=mongodb://localhost:27017/rocketchat$ReplicaSet"
+fi
 
 whiptail --msgbox "Rocket.Chat successfully installed!
 
 Open http://$IP:$port in your browser and register.
 
 The first users to register will be promoted to administrator." 12 64
-
-elif [ $ARCH = arm ]
-then
-  <<SYSTEMD_FIX_NEEDED
-  sh $DIR/sysutils/services.sh Rocket.Chat "$HOME/meteor/dev_bundle/bin/node main.js" "$HOME/Rocket.Chat
-Environment=ROOT_URL=http://$IP:$port/
-Environment=PORT=$port
-Environment=MONGO_URL=mongodb://localhost:27017/rocketchat
-Environment=MONGO_OPLOG_URL=mongodb://localhost:27017/local"
-fi
-SYSTEMD_FIX_NEEDED
-
-# Add supervisor process and run the server
-#if [ $ARCH = amd64 ] || [ $ARCH = 86 ]
-#  then node="node main.js"
-#elif [ $ARCH = arm ]
-#  then node="$HOME/meteor/dev_bundle/bin/node main.js"
-#fi
-# Install supervisor if not already present
-hash supervisorctl 2>/dev/null || $install supervisor
-
-# Create supervisor service
-cat > /etc/supervisor/conf.d/Rocket.Chat.conf <<EOF
-[program:Rocket.Chat]
-command=sh -c "ROOT_URL=http://$IP:$port/ PORT=$port MONGO_URL=mongodb://localhost:27017/rocketchat MONGO_OPLOG_URL=mongodb://localhost:27017/local $HOME/meteor/dev_bundle/bin/node main.js"
-directory=$HOME/Rocket.Chat
-autostart=true
-autorestart=unexpected
-user=$USER
-stderr_logfile=/var/log/Rocket.Chat.err.log
-stdout_logfile=/var/log/Rocket.Chat.out.log
-EOF
-supervisorctl reread
-supervisorctl update
-
-whiptail --msgbox "Rocket.Chat successfully installed!
-
-Open http://$IP:$port in your browser and register.
-The first users to register will be promoted to administrator.
-
-You can use this following command to manage the Rocket.Chat process
-supervisorctl {start|stop|status} Rocket.Chat
-
-For the logs, look in /var/log/Rocket.Chat*" 16 80
-fi
