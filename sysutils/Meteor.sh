dbf15483bf62f13a52cf7f160466838dc28fcce9
Julien Reichardt <jul.reich43@opmbx.org>
2016-06-23 09:28:29 +0200

    Fix typo

--- sysutils/Meteor.sh
+++ sysutils/Meteor.sh
@@ -14,7 +14,7 @@ elif [ $ARCHf = x86 ] ;then
 # https://github.com/4commerce-technologies-AG/meteor
 elif [ $ARCHf = arm ] ;then
   cd /usr/share
-  ggit clone --depth 1 -b release-1.2.1-universal https://github.com/4commerce-technologies-AG/meteor
+  git clone --depth 1 -b release-1.2.1-universal https://github.com/4commerce-technologies-AG/meteor
 
   # Fix curl CA error
   echo insecure > ~/.curlrc
