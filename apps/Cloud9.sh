018948f42e89f6f568059c1009333219287c5af5
Julien Reichardt <jul.reich43@opmbx.org>
2017-01-19 11:26:58 +0100

    Fix Cloud9 instructions

--- apps/Cloud9.sh
+++ apps/Cloud9.sh
@@ -1,34 +1,38 @@
 #!/bin/sh
 
-# Remove the old server executables
-[ "$1" = update ] && { git pull origin master; scripts/install-sdk.sh; chown -R cloud9: /home/cloud9; whiptail --msgbox "Cloud9 updated!" 8 32; exit;}
-[ "$1" = remove ] && { sh sysutils/service.sh remove Cloud9; userdel -rf cloud9; groupdel cloud89; whiptail --msgbox "Cloud9 removed." 8 32; break; }
+[ "$1" = update ] && { cd /home/cloud9; git pull origin master; sudo -u cloud9 scripts/install-sdk.sh; whiptail --msgbox "Cloud9 updated!" 8 32; exit;}
+[ "$1" = remove ] && { sh sysutils/service.sh remove Cloud9; rm -f /home/cloud9; userdel -rf cloud9; groupdel cloud9; whiptail --msgbox "Cloud9 removed." 8 32; break; }
 
 # Defining the port
 port=$(whiptail --title "Cloud9 port" --inputbox "Set a port number for Cloud9" 8 48 "8181" 3>&1 1>&2 2>&3)
-[ "$port" != "" ] && port_arg=" --port $port"
+port=${:-8181}
 
-. sysutils/Node.js.sh
+. apps/Node.js.sh
+
+$install gcc make sudo
 
 # Add cloud9 user
-useradd -mrU cloud9
+useradd -rU cloud9
 
-git clone git://github.com/c9/core.git c9sdk
-cd c9sdk
-scripts/install-sdk.sh
+cd /home
+git clone https://github.com/c9/core cloud9
 
 # Change the owner from root to ghost
-chown -R cloud9: /home/cloud9
+chown -R cloud9: cloud9
+
+# Install the sdk
+cd cloud9
+sudo -u cloud9 scripts/install-sdk.sh
+
+[ $IP = $LOCALIP ] && access=$IP || access=0.0.0.0
 
 # Add a systemd service and run the server
-sh sysutils/service.sh Cloud9 "/usr/bin/node server$port_arg" /home/cloud9 cloud9
+sh $DIR/sysutils/service.sh Cloud9 "/usr/bin/node server.js -p $port -l $access -a" /home/cloud9 cloud9
 
 # Start the service and enable it to start at boot
 systemctl start cloud9
 systemctl enable cloud9
 
 whiptail --msgbox "Cloud9 installed!
-
 Open http://$URL:$port in your browser and register.
-
 The first users to register will be promoted to administrator." 12 64
